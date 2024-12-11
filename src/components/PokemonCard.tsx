f2e7d6c9aea81f1cda29d5663e1ca6509ef42ede
sbg224 <sambah450@gmail.com>
2024-12-11 15:24:11 +0100

    affichage pokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+	return (
+		<figure>
+			<img
+				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				alt="Le pokemon Bulbasaur"
+			/>
+			<figcaption>
+				<p>"Bulbasaur"</p>
+			</figcaption>
+		</figure>
+	);
+}
+
+export default PokemonCard;

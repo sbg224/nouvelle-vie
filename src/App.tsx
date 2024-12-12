3ab0473e73447b130f6a5da4b17bebaca9c150ef
sbg224 <sambah450@gmail.com>
2024-12-12 20:00:12 +0100

    utilisation useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import MyTitle from "./components/MyTitle";
+import { useState } from "react";
+
 
 const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+{
+	name: "bulbasaur",
+	imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
-		name: "mew",
+	name: "charmander",
+	imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
 	},
 	{
-		name: "MoussaThouuu",
-		imgSrc:
-			"https://cdn.pixabay.com/photo/2020/07/21/16/10/pokemon-5426712_640.png",
+	name: "squirtle",
+	imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
 	},
+	{
+	name: "pikachu",
+	imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
+	name: "mew",
+},
 ];
+
+
 function App() {
-	return (
-		<div>
-			<MyTitle />
-			<PokemonCard pokemon ={pokemonList[2]} />
-		</div>
+const [pokemonIndex, setPokemonIndex] = useState(0);
+
+// Fonction pour aller au Pokémon suivant
+const handleNext = () => {
+	setPokemonIndex((pokemonIndex) =>
+	pokemonIndex === pokemonList.length - 1 ? 0 : pokemonIndex + 1
 	);
+};
+
+// Fonction pour aller au Pokémon précédent
+const handlePrevious = () => {
+	setPokemonIndex((pokemonIndex) =>
+	pokemonIndex === 0 ? pokemonList.length - 1 : pokemonIndex - 1
+	);
+};
+
+return (
+	<div>
+	<MyTitle />
+	<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+	<button type="button" onClick={handlePrevious}>
+		Précédent
+	</button>
+	<button type="button" onClick={handleNext}>
+		Suivant
+	</button>
+	</div>
+);
 }
 
 export default App;
